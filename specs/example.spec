7fd42e9a8cd374055e82449233420b47823d7eb8
azumber <ahmet.zumberoglu@modanisa.com>
2022-03-20 00:37:43 +0300

    [01][Ahmet/Enes] Acceptence criterias handled

---
+++ specs/example.spec
@@ -0,0 +1,27 @@
+# Open application
+* Open todo application
+
+## Add to do
+* Write a "buy some milk" to text box
+* Click to "Save" button
+* I should see "buy some milk" item 
+
+
+## Must list only active tasks
+* Add todos 
+   |description|
+   |-----------|
+   |first todo |
+   |second todo|
+   |third todo |
+   |fourth todo|
+   |fifth todo |
+
+* Must have 
+   |description|
+   |-----------|
+   |first todo |
+   |third todo |
+   |fourth todo|
+
+
---
+++ tests/step_implementation.js
@@ -0,0 +1,65 @@
+/* globals gauge*/
+"use strict";
+const path = require('path');
+const {
+    openBrowser,
+    write,
+    closeBrowser,
+    goto,
+    press,
+    button,
+    screenshot,
+    above,
+    click,
+    checkBox,
+    listItem,
+    toLeftOf,
+    link,
+    text,
+    into,
+    textBox,
+    evaluate
+} = require('taiko');
+const assert = require("assert");
+const headless = process.env.headless_chrome.toLowerCase() === 'true';
+
+beforeSuite(async () => {
+    await openBrowser({
+        headless: headless
+    })
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+step("Open todo application", async function () {
+    await goto("localhost:3000");
+});
+
+step("Write a <item> to text box", async (item) => {
+    await write(item, into(textBox({ id:'todo-input' })));
+});
+
+step("Click to <item> button", async function(item) {
+	await press(item);
+    await clear(textBox({ id:'todo-input' }))
+});
+
+step("I should see <message> item", async function (message) {
+    assert.ok(await text(message).exists(0, 0));
+});
+
+step("Add todos <table>", async function (table) {
+    for (var row of table.rows) {
+        await write(row.cells[0], into(textBox({ id:'todo-input' })));
+        await press('Save');
+    }
+});
+
+step("Must have <table>", async function (table) {
+    for (var row of table.rows) {
+        assert.ok(await text(row.cells[0]).exists());
+    }
+});
+
